b16a604d932d9dd5d7b467338a3c84a28ad81afd
AlanTrudor <70951157+AlanTrudor@users.noreply.github.com>
2023-07-13 02:41:50 +0000

    Fixed the chrome_options TypeError and added in an instructions file to setup headless chromedriver via terminal

--- newsfetch/helpers.py
+++ newsfetch/helpers.py
@@ -31,7 +31,7 @@ def dataframe_data(df):
 
 
 def get_chrome_web_driver(options):
-    return webdriver.Chrome(chrome_options=options)
+    return webdriver.Chrome(options=options)
 
 
 def get_web_driver_options():
