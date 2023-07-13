4a233c7dcd6f9b16cef531fbfe2231a50b5852d3
AlanTrudor <70951157+AlanTrudor@users.noreply.github.com>
2023-07-13 03:52:52 +0000

    Added in options to fix crashes and also included better error handling to avoid infinite loops

--- newsfetch/helpers.py
+++ newsfetch/helpers.py
@@ -4,50 +4,43 @@ from newsfetch.utils import chromedriver_binary, json, pd, unidecode, webdriver
 The Below functions contains error handling, unidecode, digits extraction, dataframe cleaning
 """
 
-errors = {'None': None, 'list': [], 'dict': {}}
+errors = {"None": None, "list": [], "dict": {}}
 
-
-def catch(default, func, handle=lambda e: e, *args, **kwargs):
+def catch(default, func, handle=lambda e: e,*args,**kwargs):
     try:
-        return func(*args, **kwargs)
-    except:
+        return func(*args,**kwargs)
+    except Exception as e:
+        print(f"Caught an exception: {e}")
         return errors[default]
-
 
 def unicode(text: str) -> bool:
     return unidecode.unidecode(text).strip()
 
-
 def news_article(text):
-    return unicode(' '.join(text.replace('’', '').split()))
-
+    return unicode(" ".join(text.replace("’", "").split()))
 
 def digits(text: str) -> bool:
-    return int(''.join(i for i in text if i.isdigit()))
-
+    return int("".join(i for i in text if i.isdigit()))
 
 def dataframe_data(df):
-    return df.dropna(how='all').reset_index(drop=True)
-
+    return df.dropna(how="all").reset_index(drop=True)
 
 def get_chrome_web_driver(options):
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     return webdriver.Chrome(options=options)
-
 
 def get_web_driver_options():
     return webdriver.ChromeOptions()
 
-
 def set_ignore_certificate_error(options):
-    options.add_argument('--ignore-certificate-errors')
-
+    options.add_argument("--ignore-certificate-errors")
 
 def set_browser_as_incognito(options):
-    options.add_argument('--incognito')
-
+    options.add_argument("--incognito")
 
 def set_automation_as_head_less(options):
-    options.add_argument('--headless')
+    options.add_argument("--headless")
 
 
 def author(soup):
@@ -92,6 +85,7 @@ def category(soup):
         if '' != category or i == 3:
             break
         i += 1
+    return category
 
 
 def publisher(soup):
@@ -106,5 +100,4 @@ def publisher(soup):
         if '' != publisher or i == 3:
             break
         i += 1
-
     return publisher
